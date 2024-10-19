5188e426184e553772d34418ca25428da36c929c
Viktor Apoyan <vapoyan@gmail.com>
2024-10-19 15:09:01 +0200

    Renamed application main activity (entry point) to GlucostatsActivity

--- app/src/main/java/app/glucostats/GlucostatsActivity.kt
+++ app/src/main/java/app/glucostats/GlucostatsActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.glucostats.ui.theme.GlucostatsTheme
 
-class MainActivity : ComponentActivity() {
+class GlucostatsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()

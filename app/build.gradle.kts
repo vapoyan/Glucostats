2fa4f461b65546dc087869203c1073a78d4fb1a4
Viktor Apoyan <vapoyan@gmail.com>
2024-10-21 09:42:14 +0200

    Staging v.1.0.1 (4) Handles http and https urls for glucostats.app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.glucostats"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

80a94a4464e607c3b7b9692d3b989d0571442ef0
Viktor Apoyan <vapoyan@gmail.com>
2024-10-24 21:28:12 +0200

    Staging v.1.0.3 (6) Dexcom authentication supported

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.glucostats"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
